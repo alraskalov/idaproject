166a1fb3f28d61b297d0460f7748c3db4d5ce684
Alexander <raskalovalexander@gmail.com>
2022-06-11 16:19:59 +0300

    create ButtonItem component

---
+++ components/ButtonItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button button_active">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonItem',
+  props: {
+    title: {
+      type: String,
+      default: 'Добавить товар',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 600;
+  font-size: .75rem;
+  line-height: 1.25;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #B4B4B4;
+  background: #EEE;
+  border-radius: .625rem;
+  border: none;
+  width: 284px;
+  height: 36px;
+  padding: 0;
+  &_active {
+    background: #7BAE73;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+    color: #FFFFFF;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <Tutorial />
+  <!-- <Tutorial /> -->
+  <ButtonItem />
 </template>
 
 <script>
