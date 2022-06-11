1176b18cd6f4002162745425c47e644c52858540
Alexander <raskalovalexander@gmail.com>
2022-06-11 22:37:57 +0300

    create MainComp component

---
+++ components/MainComp.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="content">
+    <h1>asdasdasd</h1>
+    <h1>asdasdasd</h1>
+    <FormItem name="create" />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'MainComp',
+}
+</script>
+
+<style>
+.content {
+  display: grid;
+  grid-gap: 1rem 1rem;
+  grid-template-columns: 440px 1fr;
+  grid-template-rows: auto auto;
+}
+
+#__layout {
+  background: rgba(255, 254, 251, 0.8);
+  text-rendering: optimizeLegibility;
+  margin: auto;
+  max-width: 1440px;
+  min-width: 320px;
+  position: relative;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,9 @@
 <template>
-  <!-- <Tutorial /> -->
-  <!-- <ButtonItem /> -->
-  <FormItem name="create" />
+  <MainComp />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: "IndexPage",
 }
 </script>
